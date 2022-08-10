d3e51daeb653c4e48d14fe115200df54b99788f0
Jose Herrera <jherrera892016@gmail.com>
2022-08-10 18:04:43 -0500

    refactor(core): change definition var for let
    - Delete unnecessary variable var i=0
    
    Co-authored-by: XimenaV26 <xvelasco1@ucol.mx>
    Co-authored-by: Ch4s3d <ramozchris@gmail.com>

--- BowlingGame/BowlingGame.js
+++ BowlingGame/BowlingGame.js
@@ -1,100 +1,97 @@
-module.exports = class Game{
-    constructor() {
-        this.player = new Player();
-        this.frames = 10;
+module.exports = class Game {
+  constructor() {
+    this.player = new Player();
+    this.frames = 10;
+  }
+
+  createPlayerFrame() {
+    this.player.frame = new Array(this.frames);
+    for (let i = 0; i < this.frames; i++) {
+      this.player.frame[i] = new Array(3);
+    }
+  }
+
+  initializePlayerFrame() {
+    for (let i = 0; i < this.frames; i++) {
+      for (let j = 0; j <= 3; j++) {
+        this.player.frame[i][j] = 0;
+      }
+    }
+  }
+
+  addScores(sentFrame) {
+    for (let i = 0; i < this.frames; i++) {
+      this.player.frame[i] = sentFrame[i];
+    }
+    return this.player.frame;
+  }
+
+  addRandomScores() {
+    let scores = new Array(this.frames);
+    for (let i = 0; i < this.frames; i++) {
+      scores[i] = new Array(3);
     }
 
-    createPlayerFrame() {
-        this.player.frame = new Array(this.frames);
-        for (var i = 0; i < this.frames; i++) {
-            this.player.frame[i] = new Array(3);
-        }
+    for (let i = 0; i < this.frames; i++) {
+      let firstShot = Math.floor(Math.random() * 10);
+      scores[i][0] = firstShot;
+      scores[i][1] = Math.floor(Math.random() * Math.abs(firstShot - 10));
+      scores[i][2] = 0;
+      scores[i][3] = 0;
+      if (
+        i == this.frames - 1 &&
+        this.IsStrike(scores[i] || this.IsSpare(scores[i]))
+      ) {
+        scores[i][2] = Math.floor(Math.random() * 10);
+      }
     }
 
-    initializePlayerFrame() {
-        for (var i = 0; i < this.frames; i++){
-            for (var j = 0; j <= 3; j++){
-                this.player.frame[i][j] = 0;
-            }
-        }
+    return scores;
+  }
+
+  game() {
+    for (let i = 0; i < this.frames; i++) {
+      this.rules(this.player.frame[i], i);
+    }
+    console.table(this.player.frame);
+    return this.player.frames;
+  }
+
+  rules(frame, i) {
+    let score = this.player.finalScore;
+
+    score += frame[0] + frame[1];
+    if (i < this.frames - 1) {
+      if (this.IsStrike(frame)) {
+        //STRIKE
+        score += this.player.frame[i + 1][0] + this.player.frame[i + 1][1];
+      } else if (this.IsSpare(frame)) {
+        //SPARE
+        score += this.player.frame[i + 1][0];
+      }
+    } else {
+      score += frame[2];
     }
 
-    addScores(sentFrame) {
-        var i = 0;
-        for (var i = 0; i < this.frames; i++){
-                this.player.frame[i] = sentFrame[i];
-        }
-        return this.player.frame;
-    }
+    this.player.finalScore = score;
+    this.player.frame[i][3] = score;
 
-    addRandomScores() {
-        var scores = new Array(this.frames);
-        for (var i = 0; i < this.frames; i++) {
-            scores[i] = new Array(3);
-        }
+    return score;
+  }
 
-        for (var i = 0; i < this.frames; i++){
-            var firstShot = Math.floor(Math.random() * 10);
-            scores[i][0] = firstShot;
-            scores[i][1] = Math.floor(Math.random() * (Math.abs(firstShot - 10))) ;
-            scores[i][2] = 0;
-            scores[i][3] = 0;
-            if (i == this.frames - 1 && (this.IsStrike(scores[i] || this.IsSpare(scores[i])))) {
-                scores[i][2] = Math.floor(Math.random() * 10);
-            }
-        }
+  IsStrike(frame) {
+    if (frame[0] == 10) return true;
+    else return false;
+  }
+  IsSpare(frame) {
+    if (frame[0] != 10 && frame[0] + frame[1] == 10) return true;
+    else return false;
+  }
+};
 
-        return scores;
-    }
-
-    game() {
-        for (var i = 0; i < this.frames; i++){
-            this.rules(this.player.frame[i], i);
-        }
-        console.table(this.player.frame);
-        return this.player.frames;
-    }
-
-    rules(frame, i) {
-        var score = this.player.finalScore;
-        
-        score += frame[0] + frame[1];
-        if(i<this.frames-1){
-            if (this.IsStrike(frame)) {
-                //STRIKE    
-            score += (this.player.frame[i+1][0] + this.player.frame[i+1][1]);
-            }
-            else if (this.IsSpare(frame)) {
-                //SPARE
-                score += (this.player.frame[i+1][0]);
-            }
-        }
-        else {
-            score += frame[2];
-        }
-
-        this.player.finalScore = score
-        this.player.frame[i][3] = score;
-
-        return score;
-    }
-
-    IsStrike(frame) {
-        if (frame[0] == 10)
-            return true;
-        else return false;
-    }
-    IsSpare(frame) {
-        if (frame[0] != 10 && frame[0]+frame[1]==10)
-            return true;
-        else return false;
-    }
-
-}
-
-class Player{
-    constructor() {
-        this.frame = [,];
-        this.finalScore = 0;
-    }
+class Player {
+  constructor() {
+    this.frame = [,];
+    this.finalScore = 0;
+  }
 }
--- BowlingGame/BowlingGame.test.js
+++ BowlingGame/BowlingGame.test.js
@@ -1,25 +1,36 @@
-const BG = require('./BowlingGame');
-var scores = [];
-var bg = undefined;
+const BG = require("./BowlingGame");
+let scores = [];
+let bg = undefined;
 
 beforeEach(() => {
   bg = new BG();
   bg.createPlayerFrame();
   bg.initializePlayerFrame();
-})
+});
 
 afterEach(() => {
   bg.game();
   scores = [];
   bg = undefined;
-})
+});
 
-test('adds score', () => {
-  scores = [[1, 4, 0, 0], [4, 5, 0, 0], [6, 4, 0, 0], [5, 5, 0, 0], [10, 0, 0, 0], [0, 1, 0, 0], [7, 3, 0, 0], [6, 4, 0, 0], [10, 0, 0, 0], [2, 8, 6, 0]];
+test("adds score", () => {
+  scores = [
+    [1, 4, 0, 0],
+    [4, 5, 0, 0],
+    [6, 4, 0, 0],
+    [5, 5, 0, 0],
+    [10, 0, 0, 0],
+    [0, 1, 0, 0],
+    [7, 3, 0, 0],
+    [6, 4, 0, 0],
+    [10, 0, 0, 0],
+    [2, 8, 6, 0],
+  ];
   expect(bg.addScores(scores)).toStrictEqual(scores);
 });
 
-test('adds random score', () => {
+test("adds random score", () => {
   scores = bg.addRandomScores();
   expect(bg.addScores(scores)).toStrictEqual(scores);
 });
