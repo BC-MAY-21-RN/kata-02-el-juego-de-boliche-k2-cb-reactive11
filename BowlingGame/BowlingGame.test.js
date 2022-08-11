0dbb0f69f8ef523f8532b91cbb117283e8287775
XimenaV26 <54459424+XimenaV26@users.noreply.github.com>
2022-08-10 19:20:27 -0500

    test(core): add new tests
    -Add testing "Check rules, Checks if frame changes, check isSpare"
    
    Co-Authored-By: Christopher R. <74276682+Ch4s3d@users.noreply.github.com>
    Co-Authored-By: Jose Herrera <66761042+featlast@users.noreply.github.com>

--- BowlingGame/BowlingGame.test.js
+++ BowlingGame/BowlingGame.test.js
@@ -56,9 +56,36 @@ test("adds random score", () => {
   expect(bg.addScores(scores)).toStrictEqual(scores);
 });
 
+test("Checks if frame changes", () => {
+  let testFrame = new Array(10);
+  for (let i = 0; i < 10; i++) {
+    testFrame[i] = new Array(3);
+  }
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j <= 3; j++) {
+      testFrame[i][j] = 0;
+    }
+  }
+  expect(bg.game()).not.toStrictEqual(testFrame);
+});
+
+test("Check rules", () => {
+ bg.addScores(bg.addRandomScores());
+  expect(bg.rules(bg.player.frame[6],6)).not.toBe(0);
+});
+
 test("check isStrike", () => {
   let testFrame = new Array(2);
   testFrame[0] = 10;
   testFrame[1] = 0;
   expect(bg.IsStrike(testFrame)).toBe(true);
 });
+
+test("check isSpare", () => {
+  let testFrame = new Array(2);
+  testFrame[0] = 5;
+  testFrame[1] = 5;
+  expect(bg.IsSpare(testFrame)).toBe(true);
+});
+
+
